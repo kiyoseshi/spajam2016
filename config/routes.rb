e09625d0f049d330a2b49ac6a00d3c46ec591a73
hirotaka-sasaki <hirotaka022105@gmail.com>
2016-05-22 06:01:50 +0900

    ボットの画面をrootに設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'chats#index'
   resources :chats
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
