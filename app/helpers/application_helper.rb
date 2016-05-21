d8d9c7b20130f0237fe1af53c3c52ba33673df16
hirotaka-sasaki <hirotaka022105@gmail.com>
2016-05-21 16:44:48 +0900

    不要なコードを削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,1 @@
-module ApplicationHelper
-  def first_entity_value(entities, entity)
-    return nil unless entities.has_key? entity
-    val = entities[entity][0]['value']
-    return nil if val.nil?
-    return val.is_a?(Hash) ? val['value'] : val
-  end
-end
+
